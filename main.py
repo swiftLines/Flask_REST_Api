1eee37aa2746248aa46f478455e07a004aecabfd
Jeremy Underwood <underwood.j248@gmail.com>
2024-01-10 20:27:26 -0500

    add get request

--- main.py
+++ main.py
@@ -4,5 +4,9 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-if __name__ == "__main__":
+class HelloWorld(Resource):
+  def get(self):
+    return {'Hello World'}
+
+if __name__ == '__main__':
   app.run(debug=True)
