523533cde7eebc6768921eb1d76b47eadb135ef9
Jeremy Underwood <underwood.j248@gmail.com>
2024-01-13 22:21:58 -0500

    add names

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
+names = {'jon': {'age': 24, 'gender': 'male'},
+         'mac': {'age': 70, 'gender': 'male'}}
+
 class HelloWorld(Resource):
-  def get(self, name, test):
-    return {'name': name, 'test':test}
+  def get(self, name):
+    return names[name]
   
   def post(self):
     return {'data': 'Posted'}
 
-api.add_resource(HelloWorld, '/helloworld/<string:name>/<int:test>')
+api.add_resource(HelloWorld, '/helloworld/<string:name>')
 
 if __name__ == '__main__':
   app.run(debug=True)
