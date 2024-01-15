78eaeb327e37bac2ecca17202aab566bdecb17a1
Jeremy Underwood <underwood.j248@gmail.com>
2024-01-14 21:46:49 -0500

    add resource and put request

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ class Video(Resource):
   def get(self, video_id):
     return videos[video_id]
   
-  def post(self):
-    return {'data': 'Posted'}
+  def put(self, video_id):
+    return{}
 
-api.add_resource(HelloWorld, '/helloworld/<string:name>')
+api.add_resource(Video, '/video/<int:video_id>')
 
 if __name__ == '__main__':
   app.run(debug=True)
